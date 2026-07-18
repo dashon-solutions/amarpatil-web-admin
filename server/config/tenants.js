31da0c098beea5aae6ee8be86d242606ebdcef84
onkar surwase <surwaseonkar12345@gmail.com>
2026-07-18 13:33:05 +0530

    test

--- server/config/tenants.js
+++ server/config/tenants.js
@@ -10,7 +10,7 @@ module.exports = {
     domains: [
       "amarpatil-web-admin-q7qq.vercel.app",
       "amarpatil-web-admin.vercel.app",
-      "studio.dashonsolutions.com",
+      "dashonsolutions.com",
     ],
     MONGODB_URI: "mongodb+srv://clientdeskteam_db_user:WDcf17sFy2efzPEq@amarpatilfinal.bzkt5bn.mongodb.net",
     ADMIN_EMAIL: "amarpatil@gmail.com",
